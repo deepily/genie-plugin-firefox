9d72c3577a920539b13b997a02d3ddd7bf56743b
Deepily <42214371+deepily@users.noreply.github.com>
2023-04-04 14:53:58 -0400

    Create first working prototype of transcription prototype spawned by keyboard shortcut. TODO: Clean this ugly mess up, LoL

--- js/background-context-menu.js
+++ js/background-context-menu.js
@@ -2,11 +2,133 @@ console.log( "background-context-menu.js loading..." );
 
 // ¡OJO! TODO: These constants should be declared globally and ultimately in a runtime configurable configuration service provided by the browser.
 // ¡OJO! TODO: background-context-menu.js and recorder.js both make duplicate declarations of these constants.
-
-// port 5000 used when running flask locally within PyCharm
-// const genieInTheBoxServer = "http://127.0.0.1:5000";
 const genieInTheBoxServer = "http://127.0.0.1:7999";
 const ttsServer = "http://127.0.0.1:5002";
+
+console.log( "NEW! background-context-menu.js loading... Done!" );
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log("error creating item:" + browser.runtime.lastError);
+  } else {
+    console.log("item created successfully");
+  }
+}
+function onError() {
+    console.log( "Error:" + browser.runtime.lastError );
+}
+// browser.contextMenus.create({
+//   id: "radio-green",
+//   type: "radio",
+//   title: "Make it green",
+//   contexts: ["all"],
+//   checked: false
+// }, onCreated);
+// browser.contextMenus.create({
+//   id: "radio-blue",
+//   type: "radio",
+//   title: "Make it blue",
+//   contexts: ["all"],
+//   checked: false
+// }, onCreated);
+browser.contextMenus.create({
+  id: "radio-popup",
+  title: "Popup",
+  contexts: ["all"],
+  checked: false
+}, onCreated);
+
+var makeItBlue = 'document.body.style.border = "5px solid blue"';
+var makeItGreen = 'document.body.style.border = "5px solid green"';
+
+// window.addEventListener('load', function ()  {
+//     document.addEventListener('keypress', function (e) {
+//         console.log( e );
+//     }, true);
+// }, false);
+
+// const input = document.querySelector("input");
+//
+// input.addEventListener("keyup", logKey);
+//
+// function logKey(e) {
+//     console.log( e.code );
+// }
+
+// document.addEventListener( "keyup", (event) => {
+//     // if (event.isComposing || event.keyCode === 229) {
+//     //   return;
+//     // }
+//     // do something
+//     console.log( "Key pressed [" + event.key + "]" );
+// });
+function showPopup ( info ){
+
+    // // var popup = 'alert("Hello world!")';
+    // var widgetWithFocus = document.activeElement;
+    // console.log( "widgetWithFocus [" + widgetWithFocus + "]" );
+    // console.log( "info.target [" + info.target + "]" );
+    // document.activeElement.style.border = "5px solid blue";
+    // console.log( document.getSelection().toString() );
+    //
+    // console.log( "info.selectionText [" + info.selectionText + "]" );
+    //
+    // // foo = window.getSelection().toString();
+    // // console.log( "foo [" + foo + "]" );
+    //
+    // // document.getSelection().empty()
+    // // info.selectionText = "";
+    // // info.selectionText.deleteFromDocument()
+    // if ( window.getSelection ) {
+    //     console.log( "window.getSelection() [" + window.getSelection() + "]" );
+    //     window.getSelection().removeAllRanges()
+    // } else if ( document.selection ) {
+    //     document.selection.empty();
+    //     console.log( "document.selection [" + document.selection + "]" );
+    // }
+
+    var popupURL = browser.runtime.getURL( "../html/recorder.html" );
+
+    let creating = browser.windows.create({
+        url: popupURL,
+        type: "popup",
+        height: 15, // Browser will force this to be a certain Minimum height
+        width: 250
+    });
+    creating.then(onCreated, onError);
+};
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+    // if (info.menuItemId == "radio-blue") {
+    //   browser.tabs.executeScript(tab.id, {
+    //     code: makeItBlue
+    //   });
+    // } else if (info.menuItemId == "radio-green") {
+    //   browser.tabs.executeScript(tab.id, {
+    //     code: makeItGreen
+    //   });
+    // } else
+    if (info.menuItemId == "radio-popup") {
+
+        let selection = window.getSelection();
+        console.log( "Menu clicked. selection [" + selection + "]" );
+        selection.deleteFromDocument();
+
+        browser.tabs.executeScript(tab.id, {
+            code: showPopup( info )
+        });
+    }
+});
+
+console.log( "browser.commands.onCommand.addListener ..." )
+browser.commands.onCommand.addListener((command) => {
+
+    console.log( "command [" + command + "]" )
+
+    if (command === "popup-vox-to-text") {
+        console.log("Popping up recorder.html..." );
+        showPopup( null )
+    }
+});
+console.log( "browser.commands.onCommand.addListener ... Done?" )
 
 // browser.contextMenus.create({
 //         id: "insert-modal",
@@ -17,15 +139,15 @@ const ttsServer = "http://127.0.0.1:5002";
 //     // for information on the purpose of this error capture.
 //     () => void browser.runtime.lastError,
 // );
-browser.contextMenus.create({
-        id: "whats-this-mean",
-        title: "What's this?",
-        contexts: ["selection"]
-    },
-    // See https://extensionworkshop.com/documentation/develop/manifest-v3-migration-guide/#event-pages-and-backward-compatibility
-    // for information on the purpose of this error capture.
-    () => void browser.runtime.lastError,
-);
+// browser.contextMenus.create({
+//         id: "whats-this-mean",
+//         title: "What's this?",
+//         contexts: ["selection"]
+//     },
+//     // See https://extensionworkshop.com/documentation/develop/manifest-v3-migration-guide/#event-pages-and-backward-compatibility
+//     // for information on the purpose of this error capture.
+//     () => void browser.runtime.lastError,
+// );
 // browser.contextMenus.create({
 //         id: "read-to-me",
 //         title: "Read to me",
@@ -37,74 +159,74 @@ browser.contextMenus.create({
 // );`
 
 
-function insertCss() {
-
-    browser.tabs.query({currentWindow: true, active: true}).then(async (tabs) => {
-
-        let tab = tabs[0]; // Safe to assume there will only be one result
-
-        console.log( "background-context-menu.js inserting CSS...[" + tab + "]");
-        try {
-            // Insert CSS from a file:
-            browser.tabs.insertCSS( tab.id, { file: "../css/modal.css" } )
-            document.getElementsByName( "body" ).innerHTML = "Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! ";
-            // Insert static CSS:
-            // let css = "body { border: 20px dotted pink; }";
-            // await browser.tabs.insertCSS( tab.id, {code: css} );
-        } catch (err) {
-            console.error(`failed to insert CSS: ${err}`);
-        }
-        console.log( "background-context-menu.js inserting CSS... Done!");
-    }, console.error )
-}
-browser.contextMenus.onClicked.addListener(async (info, tab) => {
-
-    if (info.menuItemId === "insert-modal") {
-
-        console.log("insert-modal clicked [" + info.selectionText + "]");
-        console.log("info: " + JSON.stringify(info));
-
-        insertModal(info);
-
-    } else if (info.menuItemId === "whats-this-mean") {
-
-        console.log("whats-this-mean clicked [" + info.selectionText + "]");
-        console.log("info: " + JSON.stringify(info));
-
-        console.log("calling fetchWhatsThisMean()...")
-        new Promise(function (resolve, reject) {
-
-            fetchWhatsThisMean(info).then((explanation) => {
-                console.log("calling fetchWhatsThisMean()... done!")
-                // console.log("explanation: " + explanation);
-                // console.log( "calling doTextToSpeech()..." )
-                // doTextToSpeech( explanation ).then( ( audio ) => {
-                //     console.log( "calling doTextToSpeech()... done!" )
-                // } );
-            });
-            // .then( ( response ) => {
-            //     response.text().then(respText => {
-            //         console.log("respText: " + respText );
-            //     });
-            // });
-
-            // doTextToSpeech( explanation )
-            // } else if ( info.menuItemId === "read-to-me" ) {
-            //
-            //     console.log( "read-to-me clicked [" + info.selectionText + "]" );
-            //     console.log( "info: " + JSON.stringify( info ) );
-            //
-            //     doTextToSpeech( info );
-            //
-            // }
-        });
-    }
-});
-insertModal = ( info ) => {
-
-    console.log( "insertModal() called..." )
-    insertCss()
-}
+// function insertCss() {
+//
+//     browser.tabs.query({currentWindow: true, active: true}).then(async (tabs) => {
+//
+//         let tab = tabs[0]; // Safe to assume there will only be one result
+//
+//         console.log( "background-context-menu.js inserting CSS...[" + tab + "]");
+//         try {
+//             // Insert CSS from a file:
+//             browser.tabs.insertCSS( tab.id, { file: "../css/modal.css" } )
+//             document.getElementsByName( "body" ).innerHTML = "Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! ";
+//             // Insert static CSS:
+//             // let css = "body { border: 20px dotted pink; }";
+//             // await browser.tabs.insertCSS( tab.id, {code: css} );
+//         } catch (err) {
+//             console.error(`failed to insert CSS: ${err}`);
+//         }
+//         console.log( "background-context-menu.js inserting CSS... Done!");
+//     }, console.error )
+// }
+// browser.contextMenus.onClicked.addListener(async (info, tab) => {
+//
+//     if (info.menuItemId === "insert-modal") {
+//
+//         console.log("insert-modal clicked [" + info.selectionText + "]");
+//         console.log("info: " + JSON.stringify(info));
+//
+//         insertModal(info);
+//
+//     } else if (info.menuItemId === "whats-this-mean") {
+//
+//         console.log("whats-this-mean clicked [" + info.selectionText + "]");
+//         console.log("info: " + JSON.stringify(info));
+//
+//         console.log("calling fetchWhatsThisMean()...")
+//         new Promise(function (resolve, reject) {
+//
+//             fetchWhatsThisMean(info).then((explanation) => {
+//                 console.log("calling fetchWhatsThisMean()... done!")
+//                 // console.log("explanation: " + explanation);
+//                 // console.log( "calling doTextToSpeech()..." )
+//                 // doTextToSpeech( explanation ).then( ( audio ) => {
+//                 //     console.log( "calling doTextToSpeech()... done!" )
+//                 // } );
+//             });
+//             // .then( ( response ) => {
+//             //     response.text().then(respText => {
+//             //         console.log("respText: " + respText );
+//             //     });
+//             // });
+//
+//             // doTextToSpeech( explanation )
+//             // } else if ( info.menuItemId === "read-to-me" ) {
+//             //
+//             //     console.log( "read-to-me clicked [" + info.selectionText + "]" );
+//             //     console.log( "info: " + JSON.stringify( info ) );
+//             //
+//             //     doTextToSpeech( info );
+//             //
+//             // }
+//         });
+//     }
+// });
+// insertModal = ( info ) => {
+//
+//     console.log( "insertModal() called..." )
+//     insertCss()
+// }
 
 fetchWhatsThisMean = async (info) => {
 
@@ -161,4 +283,4 @@ doTextToSpeech = async (text) => {
     console.log("doTextToSpeech() called... done!")
 }
 
-console.log( "background-context-menu.js loading... Done!" );
+console.log( "NEW!  background-context-menu.js loading... Done!" );
