30c8b15befc89344c67a204477c490651352df44
Deepily <42214371+deepily@users.noreply.github.com>
2023-04-11 17:22:06 -0400

    Add pop-ups functionality to menu bar

--- js/menubar.js
+++ js/menubar.js
@@ -1,31 +1,133 @@
 // import "genie-utils.js"
 
-window.addEventListener( "DOMContentLoaded", (event) => {
-    document.getElementById("option-1").addEventListener("click", (event) => {
-        // alert("option-1")
-        showRecorderPopup()
-    } );
-    document.getElementById("option-1").focus()
-} );
+const ttsServer = "http://127.0.0.1:5002";
+const genieInTheBoxServer = "http://127.0.0.1:7999";
+
 // capture key events
-document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowDown") {
-        console.log("ArrowDown")
-    }
-    if (event.key === "ArrowUp") {
-        console.log("ArrowUp")
-    }
+document.addEventListener("keypress", (event) => {
+
+    console.log( "key pressed: " + event.key );
+
+    // if (event.key === "ArrowDown") {
+    //
+    //     console.log("ArrowDown")
+    //     currentFocus = document.activeElement;
+    //     for ( button in buttons ) {
+    //         if ( button === currentFocus ) {
+    //             console.log( "button has focus: " + button )
+    //             break;
+    //         }
+    //     }
+    // } else if (event.key === "ArrowUp") {
+    //     console.log("ArrowUp")
+    // }
 } );
 
-function showRecorderPopup (info ){
 
-    var popupURL = browser.runtime.getURL( "../html/recorder.html" );
+document.addEventListener("click", (e) => {
 
-    let creating = browser.windows.create({
-        url: popupURL,
+    console.log( "click detected: " + e.target.id );
+
+    if ( e.target.id === "transcribe") {
+
+        popupRecorder("transcription");
+
+    } else if ( e.target.id === "command-mode" ) {
+
+        popupRecorder("multimodal editor" );
+
+    } else if ( e.target.id === "command-proofread" ) {
+
+        popupRecorder("multimodal editor proofread");
+
+    } else if ( e.target.id === "command-whats-this" ) {
+
+        fetchWhatsThisMean();
+
+    } else if ( e.target.id === "command-paste" ) {
+
+        browser.tabs.query({currentWindow: true, active: true}).then(async (tabs) => {
+        let tab = tabs[0];
+        browser.tabs.sendMessage( tab.id, {
+            command: e.target.id
+        });
+    });
+    } else {
+        console.log( "Unknown button clicked: " + e.target.id );
+    }
+    // browser.tabs.query({currentWindow: true, active: true}).then(async (tabs) => {
+    //     let tab = tabs[0];
+    //     browser.tabs.sendMessage( tab.id, {
+    //         command: e.target.id
+    //     });
+    // });
+} );
+function popupRecorder( mode ) {
+    let createData = {
+        url: "../html/recorder.html?mode=" + mode,
         type: "popup",
         height: 15, // Browser will force this to be a certain Minimum height
         width: 280
-    });
-    creating.then(onCreated, onError);
-};
+    };
+    let creating = browser.windows.create(createData);
+}
+// function listenForClicks() {
+//
+//     document.addEventListener("click", (e) => {
+//         console.log( "click detected: " + e.target.id );
+//
+//         browser.tabs.query({currentWindow: true, active: true}).then(async (tabs) => {
+//             let tab = tabs[0];
+//             browser.tabs.sendMessage( tab.id, {
+//                 command: e.target.id
+//             });
+//         });
+//     } )
+// }
+// browser.tabs.executeScript({file: "../js/content.js"})
+// .then( () => { console.log( "Script loaded..." ) } )
+// .catch(reportExecuteScriptError);
+//
+// function reportExecuteScriptError(error) {
+//     console.error(`Failed to execute content script: ${error.message}`);
+// }
+
+fetchWhatsThisMean = async () => {
+
+    console.log("fetchWhatsThisMean() called...")
+
+    const clipboardText = await navigator.clipboard.readText();
+
+    let url = genieInTheBoxServer + "/api/ask-ai-text?question=" + clipboardText
+    const encodedUrl = encodeURI(url);
+    console.log("encoded: " + encodedUrl);
+
+    await fetch(url, {
+        method: 'GET',
+        headers: {'Access-Control-Allow-Origin': '*'}
+    }).then( async (response) => {
+        console.log("response.status: " + response.status);
+        if ( response.status !== 200) {
+            return Promise.reject("Server error: " + response.status);
+        } else {
+            await response.text().then( async respText => {
+                console.log("respText: " + respText);
+                await doTextToSpeech( respText )
+            })
+        }
+    })
+}
+
+doTextToSpeech = async (text) => {
+
+    console.log("doTextToSpeech() called...")
+
+    let url = ttsServer + "/api/tts?text=" + text
+    const encodedUrl = encodeURI(url);
+    console.log("encoded: " + encodedUrl);
+
+    const audio = new Audio(encodedUrl);
+    await audio.play();
+
+    console.log("doTextToSpeech() called... done!")
+}
