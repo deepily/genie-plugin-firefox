b79408991f7fb084f0d22faaa23f2a49184e5232
Deepily <42214371+deepily@users.noreply.github.com>
2023-05-04 12:38:56 -0400

    Create global constants module

---
+++ js/constants.js
@@ -0,0 +1,14 @@
+export const TTS_SERVER = "http://127.0.0.1:5002";
+export const GIB_SERVER = "http://127.0.0.1:7999";
+
+export const ZOOM_INCREMENT = 0.075;
+export const MAX_ZOOM = 5;
+export const MIN_ZOOM = 0.3;
+export const DEFAULT_ZOOM = 1;
+
+export const CMD_SEARCH_DDG     = "search duck duck go";
+export const CMD_SEARCH_GOOGLE  = "search google";
+export const CMD_OPEN_NEW_TAB   = "open new tab";
+export const MULTIMODAL_EDITOR  = "multimodal editor";
+export const COMMAND_MODE       = "command"
+export const TRANSCRIPTION_MODE = "transcription"
