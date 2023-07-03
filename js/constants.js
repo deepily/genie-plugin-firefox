d550603882c8329284951c6a3c300c3f16363576
Deepily <42214371+deepily@users.noreply.github.com>
2023-07-03 12:42:38 -0400

    Tweak voice command constants strings

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,5 @@
 console.log( "Loading constants.js..." );
+// These constants will be parsed by the multimodal munger object, and should be listed from top to bottom by longest string first.
 
 export const VOX_CMD_CUT            = "cut";
 export const VOX_CMD_COPY           = "copy";
@@ -10,7 +11,7 @@ export const VOX_EDIT_COMMANDS   = [ VOX_CMD_CUT, VOX_CMD_COPY, VOX_CMD_PASTE, V
 export const VOX_CMD_TAB_CLOSE      = "close this tab";
 export const VOX_CMD_TAB_BACK       = "go backwards";
 export const VOX_CMD_TAB_FORWARD    = "go forwards";
-export const VOX_CMD_TAB_REFRESH    = "refresh";
+export const VOX_CMD_TAB_REFRESH    = "refresh this tab";
 export const VOX_CMD_OPEN_NEW_TAB   = "open new tab";
 export const VOX_TAB_COMMANDS    = [ VOX_CMD_TAB_BACK, VOX_CMD_TAB_FORWARD, VOX_CMD_TAB_REFRESH, VOX_CMD_TAB_CLOSE, VOX_CMD_OPEN_NEW_TAB ];
 
@@ -20,11 +21,11 @@ export const VOX_CMD_OPEN_FILE      = "open file";
 export const VOX_CMD_OPEN_URL_BUCKET= "open url bucket";
 export const VOX_CMD_PROOFREAD      = "proofread";
 export const VOX_CMD_PROOFREAD_STEM = "proof";
-export const VOX_CMD_VIEW_CONSTANTS = "view constan";
+export const VOX_CMD_VIEW_CHEAT_SHEET= "view cheat sheet";
 
-export const VOX_CMD_ZOOM_IN        = "zoom"
+export const VOX_CMD_ZOOM_IN        = "zoom in";
 export const VOX_CMD_ZOOM_OUT       = "zoom out";
-export const VOX_CMD_ZOOM_RESET     = "zoom reset"
+export const VOX_CMD_ZOOM_RESET     = "zoom reset";
 
 export const VOX_CMD_MODE_RESET     = "reset";
 export const VOX_CMD_MODE_EXIT      = "exit";
@@ -43,21 +44,22 @@ export const VOX_CMD_SEARCH_GOOGLE           = "search google";
 export const VOX_CMD_SEARCH_GOOGLE_SCHOLAR   = "search google scholar";
 export const VOX_CMD_SEARCH_CLIPBOARD_DDG    = "search using clipboard";
 export const VOX_CMD_SEARCH_CLIPBOARD_GOOGLE = "search google using clipboard";
+export const VOX_CMD_SEARCH_CLIPBOARD_GOOGLE_SCHOLAR = "search google scholar using clipboard";
 
 export const MULTIMODAL_CONTACT_INFO  = "multimodal contact information";
 export const MULTIMODAL_TEXT_PYTHON   = "multimodal python punctuation";
-export const MULTIMODAL_TEXT_EMAIL    = "multimodal text email"
+export const MULTIMODAL_TEXT_EMAIL    = "multimodal text email";
 export const STEM_MULTIMODAL_EDITOR   = "multimodal editor";
 export const STEM_MULTIMODAL_SERVER_SEARCH = "multimodal server search"
 
-export const VOX_CMD_RUN_PROMPT           = "run prompt"
+export const VOX_CMD_RUN_PROMPT           = "run prompt";
 export const VOX_CMD_SUFFIX_FROM_FILE     = "from file";
 export const VOX_CMD_SUFFIX_FROM_CLIPBOARD= "from clipboard";
 
 export const VOX_CMD_SAVE_FROM_CLIPBOARD= "save from clipboard";
 
-export const MODE_TRANSCRIPTION       = "transcription"
-export const MODE_COMMAND             = "command"
+export const MODE_TRANSCRIPTION       = "transcription mode";
+export const MODE_COMMAND             = "command mode";
 
 export const TTS_SERVER_ADDRESS        = "http://127.0.0.1:5002";
 export const GIB_SERVER_ADDRESS        = "http://127.0.0.1:7999";
@@ -66,7 +68,7 @@ export const EDITOR_URL                = "http://127.0.0.1:8080/genie-plugin-fir
 export const BUCKET_URL                = "http://127.0.0.1:8080/genie-plugin-firefox/html/blank.html";
 export const CONSTANTS_URL             = "http://127.0.0.1:8080/genie-plugin-firefox/js/constants.js";
 export const SEARCH_URL_GOOGLE         = "https://www.google.com/search";
-export const SEARCH_URL_GOOGLE_SCHOLAR = "https://scholar.google.com/scholar"
+export const SEARCH_URL_GOOGLE_SCHOLAR = "https://scholar.google.com/scholar";
 export const SEARCH_URL_DDG            = "https://www.duckduckgo.com/";
 
 export const ZOOM_INCREMENT = 0.075;
